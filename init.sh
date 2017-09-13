43223a82c55cfc64c47bcde7c3313f851d0203b5
ayato_p <ayato.skycrawlers@gmail.com>
2017-09-13 09:20:20 +0900

    Update init.sh!

--- init.sh
+++ init.sh
@@ -3,5 +3,18 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" "$POSTGRES_DB" <<-EOSQL
-create extension if not exsits "uuid-ossp";
+create schema extensions;
+
+-- make sure everybody can use everything in the extensions schema
+grant usage on schema extensions to public;
+grant execute on all functions in schema extensions to public;
+
+-- include future extensions
+alter default privileges in schema extensions
+   grant execute on functions to public;
+
+alter default privileges in schema extensions
+   grant usage on types to public;
+
+create extension if not exists "uuid-ossp" schema extensions;
 EOSQL
